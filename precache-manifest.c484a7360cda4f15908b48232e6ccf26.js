e54b486b678b89683365e0f09ed97996112e44c0
galamian <johannespardowitz@gmx.de>
2020-04-17 15:51:25 +0200

    Updates

---
+++ precache-manifest.c484a7360cda4f15908b48232e6ccf26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa83b7ca1607363fddc96700806c906",
+    "url": "/jojo-sudoku/index.html"
+  },
+  {
+    "revision": "ebdde0e7db80c4253087",
+    "url": "/jojo-sudoku/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f040362ccc9a90bfaf79",
+    "url": "/jojo-sudoku/static/js/2.5b4ac46b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jojo-sudoku/static/js/2.5b4ac46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdde0e7db80c4253087",
+    "url": "/jojo-sudoku/static/js/main.7ea3c4b6.chunk.js"
+  },
+  {
+    "revision": "1c748091ca46736183e7",
+    "url": "/jojo-sudoku/static/js/runtime-main.10a1a12c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jojo-sudoku/static/media/logo.5d5d9eef.svg"
+  }
+]);
